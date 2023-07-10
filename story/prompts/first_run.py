527a99fb62bdf887a9d921df9e5500cbc84928f6
Rollingpig <sea127@qq.com>
2023-07-09 23:26:09 -0400

    disable persona spectrum temporarily

--- story/prompts/first_run.py
+++ story/prompts/first_run.py
@@ -3,15 +3,6 @@ question_dict = {
     "character_description": "Describe the main character using two sentences, exclude description on personality.",
     "character_personality": "Describe the main character's personality using one word.",
     "personality_antonym": "What is the antonym of the personality word you just wrote? Write it in one word.",
-    "personality_spectrum_1": "Provide 5 sentences to describe that personality spectrum, "
-                              "ranging from the strongest extent of that to the strongest opposite of that. "
-                              "Write the description in direct language without justifying negative description. "
-                              "Ensure the 3th sentence is close to a neutral description."
-                              "You should only write the first sentence here.",
-    "personality_spectrum_2": "You should only write the second sentence here.",
-    "personality_spectrum_3": "You should only write the third sentence here.",
-    "personality_spectrum_4": "You should only write the fourth sentence here.",
-    "personality_spectrum_5": "You should only write the fifth sentence here.",
     "summary": "Summarize the story using four sentences.",
     "element": "What is the most interesting element in the story? Write it short.",
     "action1": "List three most important actions that the main character did. "
